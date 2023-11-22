4af64dd14a42be3af8b98ad4d4b4e8f199e6fcdd
yogesh <yugyogeshpawar@gmail.com>
2023-11-23 00:45:27 +0530

    changes

---
+++ Readme.md
@@ -0,0 +1,11 @@
+agar resource group create karna ho to
+
+----- 
+
+resource "azurerm_resource_group" "myfirstrgtf" {
+  name     = "MyTerraformResourceGroup"
+  location = "East US"
+}
+
+
+
---
+++ create-resource-group-azure/variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+
+variable "resource_group_name" {
+  type        = string
+  default     = "Yogesh_TF_RG"
+  description = "Location of the resource group."
+}
+
