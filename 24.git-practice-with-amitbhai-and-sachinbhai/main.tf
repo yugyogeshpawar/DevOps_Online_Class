3b44116ad30c6d868bf84bbdccdfd28299e556f7
Yogesh Pathe <98703537+yugyogeshpawar@users.noreply.github.com>
2024-02-01 21:25:35 +0530

    Update main.tf

--- 24.git-practice-with-amitbhai-and-sachinbhai/main.tf
+++ 24.git-practice-with-amitbhai-and-sachinbhai/main.tf
@@ -9,3 +9,12 @@ resource "azurerm_virtual_network" "yogesh-vn" {
     location            = azurerm_resource_group.yogesh-rg.location
     resource_group_name = azurerm_resource_group.yogesh-rg.name
 }
+
+
+
+#resource "azurerm_virtual_network" "yogesh-vn" {
+#    name                = "yogesh-vn12"
+#   address_space       = ["89.0.142.86/16"]
+#    location            = azurerm_resource_group.yogesh-rg.location
+#    resource_group_name = azurerm_resource_group.yogesh-rg.name
+#}
